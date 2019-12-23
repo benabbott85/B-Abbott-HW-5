1fbf8f7b6e1a35e398709647ea726bf3aa69d235
Ben Abbott <babbott85@gmail.com>
2019-12-23 14:49:29 -0700

    textboxes add to page and function written loop thru time of day

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".save").on("click", function(){
+    var value = $(this).siblings(".userInput").val();
+    var time = $(this).parent().attr("id");
+
+
+    localStorage.setItem(time, value)
+    })
+
+    function hourUpdate(){
+        var currentHour = moment().hours();
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        for (i =0; i < calendar.length; i++){
+            if (blockHour < currentHour){
+                $(this).addClass("past");
+            } else if (currentHour === blockHour){
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).addClass ("future")
+            }
+        }
+        
+    }
+
+})
+
+
+var save = localStorage.getItem();
+
+
+
