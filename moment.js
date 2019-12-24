8e8d047fe3513daecaacde38aab6acba63020d62
Ben Abbott <babbott85@gmail.com>
2019-12-23 21:41:26 -0700

    made changes using starter code provided

--- moment.js
+++
@@ -1,5 +0,0 @@
-moment().format('MMMM Do YYYY, h:mm:ss a'); // December 23rd 2019, 11:26:15 am
-moment().format('dddd');                    // Monday
-moment().format("MMM Do YY");               // Dec 23rd 19
-moment().format('YYYY [escaped] YYYY');     // 2019 escaped 2019
-moment().format();
--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-$(document).ready(function () {
-    $(".save").on("click", function(){
-    var value = $(this).siblings(".userInput").val();
-    var time = $(this).parent().attr("id");
+$(document).ready(function (event) {
+    event.preventDefault();
+    $(".save").on("click", function () {
+        var value = $(this).siblings(".userInput").val();
+        var time = $(this).parent().attr("id");
 
 
-    localStorage.setItem(time, value)
+        localStorage.setItem(time, value)
     })
 
-    function hourUpdate(){
+
+    hourUpdate();
+
+
+    function hourUpdate() {
         var currentHour = moment().hours();
         var blockHour = parseInt($(this).attr("id").split("-")[1]);
-        for (i =0; i < calendar.length; i++){
-            if (blockHour < currentHour){
+        for (i = 0; i < calendar.length; i++) {
+            if (blockHour < currentHour) {
                 $(this).addClass("past");
-            } else if (currentHour === blockHour){
+            } else if (currentHour === blockHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
             } else {
-                $(this).addClass ("future")
+                $(this).addClass("future")
+
+                localStorage.getItem(time);
+                localStorage.getItem(value);
             }
-        }
         
+        
+        }
+    
+
     }
 
 })
 
 
-hourUpdate();
+// function moment(){
+// var dateEntered = $('#txtEnteredDate').val();
 
-localStorage.getItem(time, value);
+// if (!moment(dateEntered,'MM-DD-YYYY').isValid()) {
+//   console.log('Invalid Date');
+// } else {
+//   console.log('Valid Date');
+// }
+
+// }
+
+// hourUpdate();
+
+// var rightNow = moment();
+// console.log(rightNow);
+
+// moment ();
+
+
 
 
 
