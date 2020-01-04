da62d83918c093b7cc4f3580deb2428533cfbfae
Ben Abbott <babbott85@gmail.com>
2020-01-04 12:53:44 -0700

    moment.js script calle added properly

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
 
 ## Link to Deployed Version
 - https://benabbott85.github.io/B-Abbott-HW-5/
+
+## Summary of whats occuring
+- when loading the page, you will see the planner centered up on the page below the h1 tag
+- colors of boxes will reflect time past in gray, with the current/future time boxes in green
+- will be able to input text into boxes and save to local storage
--- script.js
+++ script.js
@@ -1,50 +1,86 @@
-$(document).ready(function (event) {
-    event.preventDefault();
-    $(".save").on("click", function () {
-        var value = $(this).siblings(".userInput").val();
-        var time = $(this).parent().attr("id");
-
-
-        localStorage.setItem(time, value)
-    })
-
-
-    hourUpdate();
-
-
-    function hourUpdate() {
-        var currentHour = moment().hours();
-        var blockHour = parseInt($(this).attr("id").split("-")[1]);
-        for (i = 0; i < calendar.length; i++) {
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-            } else if (currentHour === blockHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-            } else {
-                $(this).addClass("future")
-
-                localStorage.getItem(time);
-                localStorage.getItem(value);
-            }
-        
-        
-        }
+$(document).ready(function () {
+    // get values from local storage
     
-
+    
+    for (let hour = 9; hour < 18; hour++) {
+        var item = localStorage.getItem(hour)
+        if (item !== null) {
+            // push those values into correspding input boxes
+            $('input.' + hour).val(item);
+        }
     }
 
-})
 
-var dayDate = moment().format("dddd, MMMM Do YYYY");
-var currentHour = moment().format("HH");
-// saveAry.push(dayDate);
+    
+    console.log(moment().format('H'));
+    
+    var currentHour = moment().format('H');
+   
+    
+    for (let hour = 9; hour < currentHour; hour++) {
+       
+        $("li." + hour).addClass("redBg")
+    }
 
-console.log(currentHour);
-console.log(dayDate);
+    
+    // $("li.list-group-item").addClass("redBg")
+    // }
+    
+    
+    // anything under the current hour
+   
+
+    // either
+    // create a class in you css that gives color wanted
+    // use jquery to add a class to that element
+    // or
+    // dynamically changes html
+
+
+
+    // hour = hour + 1;
+    // hour += 1;
+    // ++hour;
+
+    // hour++;
+    // for (let hour = 0900; hour < 0800 || hour > 1800; hour++) {
+    // };
+    // var time = hour
+    // item = localStorage.getItem(time)
+    // if (item !== null) {
+    //     $('.' + time + '> input').val(item)
+    // }
+    // if (hour === 1200){
+    //     hour = 0;
+    // };
 
 
 
 
+    // link moment().format result to css styling
+    // 1) get result from moment.format
+    // 2) link time >current time to css coloring
+    // 3) set css coloring for card
+    // 4) set css coloring to grey if time is <current time
+
+    // console.log(moment().format());
+
+    
 
 
+
+    // on click for save buttons
+    $('.save').on('click', function () {
+
+        // grab the text from the input box
+        // console.log(this.id);
+        var currentButtonId = this.id;
+        var text = $('input.' + currentButtonId).val();
+        // console.log(inputBox.val())
+        // push it to local storage
+        localStorage.setItem(currentButtonId, text);
+
+        // $('.' + currentButtonId).addClass('redBg');
+    })
+
+});
